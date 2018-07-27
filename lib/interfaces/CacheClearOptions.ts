68d08ede92bcdb0a36e0c420504573bca54ec519
Joshua Anderson Slate <josh.a.slate@gmail.com>
2018-07-26 19:53:10 -0500

    Updating options for cache and cache clear decorators

--- lib/interfaces/CacheClearOptions.ts
+++ lib/interfaces/CacheClearOptions.ts
@@ -1,9 +1,10 @@
 import { CacheKeyBuilder } from './CacheKeyBuilder';
 import { CacheClient } from './CacheClient';
+import { NoOpDeterminer } from './NoOpDeterminer';
 
 export interface CacheClearOptions {
   cacheKey?: string | CacheKeyBuilder;
   hashKey?: string | CacheKeyBuilder;
   client?: CacheClient;
-  noop?: boolean;
+  noop?: boolean | NoOpDeterminer;
 }
--- lib/interfaces/CacheOptions.ts
+++ lib/interfaces/CacheOptions.ts
@@ -1,10 +1,11 @@
 import { CacheKeyBuilder } from './CacheKeyBuilder';
 import { CacheClient } from './CacheClient';
+import { NoOpDeterminer } from './NoOpDeterminer';
 
 export interface CacheOptions {
   cacheKey?: string | CacheKeyBuilder;
   hashKey?: string | CacheKeyBuilder;
   client?: CacheClient;
   ttlSeconds?: number;
-  noop?: boolean;
+  noop?: boolean | NoOpDeterminer;
 }
