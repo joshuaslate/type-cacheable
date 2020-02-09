591bbb91be6488b6d796fcde290a25ae206399cb
Daniel Varnai <daniel.v@motdgd.com>
2020-02-09 18:28:29 +0100

    parseIfRequired.ts is obsolete

--- lib/util/index.ts
+++ lib/util/index.ts
@@ -1,7 +1,6 @@
 export { determineOp } from './determineOp';
 export { extractKey, getCacheKey, getFinalKey } from './getCacheKey';
 export { getTTL } from './getTTL';
-export { parseIfRequired } from './parseIfRequired';
 export { useIoRedisAdapter, useNodeCacheAdapter, useRedisAdapter } from './useAdapter';
 export { serializeValue } from './serializeValue';
 export { isScalar, SCALAR_KEY } from './isScalar';
