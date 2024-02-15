4222353f779f2f15188f99c82f0970fb351d8544
Joshua Slate <josh.a.slate@gmail.com>
2024-02-15 11:36:33 -0800

    update comment

--- packages/core/lib/util/getCacheClient.ts
+++ packages/core/lib/util/getCacheClient.ts
@@ -6,12 +6,11 @@ import { CacheClient, CacheClientBuilder } from '../interfaces';
  * @param passedInCacheClient A CacheClient, or function to build the CacheClient based on arguments/context
  * @param args                The arguments the decorated method was called with
  * @param context             The instance whose method is being called
- * 
+ *
  * @returns {CacheClient}
  */
 export const getCacheClient = (passedInCacheClient: CacheClient | CacheClientBuilder, args: any[], context?: any): CacheClient => {
-  // If the user passed in a cacheKey, use that. If it's a string/number, use it directly.
-  // In the case of a function
+  // If the user passed in a cache client, use that. If it's a function to build a cache client, call it with args and context.
   return passedInCacheClient instanceof Function
     ? passedInCacheClient(args, context)
     : passedInCacheClient;
