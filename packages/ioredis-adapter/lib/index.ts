072dfa2a1aa60a9cd34d86942c459413757f1b1a
Joshua Slate <josh.a.slate@gmail.com>
2025-09-05 11:03:41 -0700

    remove compare versions

--- packages/ioredis-adapter/lib/index.ts
+++ packages/ioredis-adapter/lib/index.ts
@@ -1,14 +1,13 @@
-import { Cluster, Redis } from 'ioredis';
-import { compareVersions } from 'compare-versions';
-import cacheManager, { CacheClient, CacheManagerOptions, parseIfRequired } from '@type-cacheable/core';
-
-const REDIS_VERSION_UNLINK_INTRODUCED = '4.0.0';
-const REDIS_VERSION_FRAGMENT_IDENTIFIER = 'redis_version:';
+import cacheManager, {
+  type CacheClient,
+  type CacheManagerOptions,
+  parseIfRequired,
+} from '@type-cacheable/core';
+import { Cluster, type Redis } from 'ioredis';
 
 export class IoRedisAdapter implements CacheClient {
   constructor(redisClient: Redis | Cluster) {
     this.redisClient = redisClient;
-    this.redisVersion = '0';
     if (this.redisClient.options.keyPrefix) {
       this.keyPrefixRe = new RegExp(`^${this.redisClient.options.keyPrefix}`);
     }
@@ -24,16 +23,12 @@ export class IoRedisAdapter implements CacheClient {
   }
 
   private async init() {
-    const infoString = await this.redisClient.info();
-    const versionFragment = infoString
-      .split('\n')
-      .find((info: string) => info.includes(REDIS_VERSION_FRAGMENT_IDENTIFIER));
-    this.redisVersion =
-      versionFragment?.replace('\r', '').split(REDIS_VERSION_FRAGMENT_IDENTIFIER)[1] || '0';
+    const res = await this.redisClient.command('INFO', 'UNLINK');
+    this.supportsUnlink = Boolean(res?.length);
   }
 
   private keyPrefixRe?: RegExp;
-  private redisVersion: string = '';
+  private supportsUnlink = true;
   private readonly redisClient: Redis | Cluster;
 
   public async get(cacheKey: string): Promise<any> {
@@ -66,7 +61,7 @@ export class IoRedisAdapter implements CacheClient {
 
     const keysToDelete = Array.isArray(keyOrKeys) ? keyOrKeys : [keyOrKeys];
 
-    if (compareVersions(this.redisVersion, REDIS_VERSION_UNLINK_INTRODUCED) >= 0) {
+    if (this.supportsUnlink) {
       return this.redisClient.unlink(...keysToDelete);
     }
 
@@ -74,21 +69,31 @@ export class IoRedisAdapter implements CacheClient {
   }
 
   public async keys(pattern: string): Promise<string[]> {
-    if(this.redisClient instanceof Cluster) {
+    if (this.redisClient instanceof Cluster) {
       const cluster = this.redisClient as Cluster;
-      const keys = cluster.nodes('master').map((node) => this.scanKeys(node, pattern));
+      const keys = cluster
+        .nodes('master')
+        .map((node) => this.scanKeys(node, pattern));
       return (await Promise.all(keys)).flat();
     }
-    return  this.scanKeys(this.redisClient, pattern);
+
+    return this.scanKeys(this.redisClient, pattern);
   }
 
-  private async scanKeys(client: Redis | Cluster, pattern: string): Promise<string[]> {
+  private async scanKeys(
+    client: Redis | Cluster,
+    pattern: string,
+  ): Promise<string[]> {
     let cursor: number | null = 0;
     let keys: string[] = [];
     while (cursor !== null) {
-      const result = (await client.scan(cursor, 'MATCH', pattern, 'COUNT', 1000)) as
-          | [string, string[]]
-          | undefined;
+      const result = (await client.scan(
+        cursor,
+        'MATCH',
+        pattern,
+        'COUNT',
+        1000,
+      )) as [string, string[]] | undefined;
 
       if (result) {
         // array exists at index 1 from SCAN command, cursor is at 0
@@ -98,19 +103,29 @@ export class IoRedisAdapter implements CacheClient {
         cursor = null;
       }
     }
-    return this.keyPrefixRe ? keys.map(key => key.replace(this.keyPrefixRe!, '')) : keys;
+    return this.keyPrefixRe
+      ? keys.map((key) => key.replace(this.keyPrefixRe!, ''))
+      : keys;
   }
 
   public async delHash(hashKeyOrKeys: string | string[]): Promise<any> {
-    const finalDeleteKeys = Array.isArray(hashKeyOrKeys) ? hashKeyOrKeys : [hashKeyOrKeys];
-    const deletePromises = finalDeleteKeys.map((key) => this.keys(`*${key}*`).then(this.del));
+    const finalDeleteKeys = Array.isArray(hashKeyOrKeys)
+      ? hashKeyOrKeys
+      : [hashKeyOrKeys];
+    const deletePromises = finalDeleteKeys.map((key) =>
+      this.keys(`*${key}*`).then(this.del),
+    );
 
     await Promise.all(deletePromises);
     return;
   }
 }
 
-export const useAdapter = (client: Redis | Cluster, asFallback?: boolean, options?: CacheManagerOptions): IoRedisAdapter => {
+export const useAdapter = (
+  client: Redis | Cluster,
+  asFallback?: boolean,
+  options?: CacheManagerOptions,
+): IoRedisAdapter => {
   const ioRedisAdapter = new IoRedisAdapter(client);
 
   if (asFallback) {
